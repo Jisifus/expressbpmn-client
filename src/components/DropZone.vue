2937d839e9df708433b337bb8b403211036d5b6f
Jisifus <jisifus@gmail.com>
2022-11-13 03:25:14 +0100

    Initial commit

---
+++ src/components/DropZone.vue
@@ -0,0 +1,68 @@
+<template>
+  <div 
+  @dragenter.prevent="toggleActive"
+  @dragleave.prevent="toggleActive"
+  @dragover.prevent
+  @drop.prevent="toggleActive"
+  :class="{'active-dropzone':active}"
+  class="dropzone">
+    <span>Drag and Drop File</span>
+    <span>OR</span>
+    <label for="dropzoneFile">Select File</label>
+    <input type="file" id="dropzoneFile" class="dropzoneFile" name="file">
+  </div>
+</template>
+
+<script>
+import {ref} from "vue"
+export default {
+  name: 'DropZone',
+  setup(){
+    const active = ref(false)
+
+    const toggleActive = () => {
+      active.value = !active.value
+    }
+    return {active, toggleActive}
+  }
+  }
+</script>
+
+<style scoped lang="scss">
+.dropzone{
+  width: 400px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 16px;
+  border: 2px dashed #41b883;
+  background: white;
+  transition: .3s ease all;
+
+  label{
+    padding: 8px 12px;
+    color: white;
+    background-color: #41b883;
+    transition: .3s ease all;
+    cursor: pointer;
+    border-radius: 10px;
+  }
+}
+input{
+  display:none
+}
+
+.active-dropzone{
+  color: white;
+  border-color: white;
+  background-color: #41b883;
+
+  label{
+    background-color: white;
+    color: #41b883;
+  }
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,86 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>BPMN Lint Analyzer</h1>
+    <DropZone @drop.prevent="drop" @change="selectedFile"/>
+    <span class="file-info">File:{{dropzoneFile.name}}</span>
+    <button @click="sendFile">Upload File</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DropZone from '@/components/DropZone.vue'
+import {ref} from "vue"
+import axios from 'axios'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    DropZone
+  },
+  setup(){
+    let dropzoneFile = ref("")
+
+    const drop = (e) => {
+      dropzoneFile.value = e.dataTransfer.files[0]
+    }
+    const selectedFile = () => {
+      dropzoneFile.value = document.querySelector('.dropzoneFile').files[0]
+    }
+    const sendFile = () => {
+      
+      // const formData = new FormData()
+      // console.log(formData)
+      // formData.append('file', dropzoneFile.value)
+      // formData.append('message', 'Hello World')
+      // fetch('http://localhost:3000/fileupload',{
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type':'multipart/form-data'
+      //   },
+      //   body: formData
+      // }).catch((error) => ("something is fucky wucky",error))
+
+      let formData = new FormData()
+      formData.append('file', dropzoneFile.value)
+
+
+      axios.post('http://localhost:3000/fileupload', formData,{
+        headers: {
+          'Content-Type':'multipart/form-data'
+        }
+      }).catch(error => {
+        console.log(error)
+      })
+
+    }
+    
+    return{dropzoneFile, drop, selectedFile, sendFile}
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.home{
+  font-family: Arial, Helvetica, sans-serif;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #f1f1f1;
+
+  h1{
+    font-size: 40px;
+    margin-bottom: 32px;
+  }
+
+  .file-info{
+    margin:top 32px;
+  }
+
+}
+button{
+  background-color: #41b883;
+  color: white;
+}
+</style>
